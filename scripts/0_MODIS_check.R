169f69326e7e44ed2933193f3de39ecf36a92d6e
Christy Rollinson <crollinson@gmail.com>
2019-04-23 16:02:55 -0500

    small updates to MODIS check

--- scripts/0_MODIS_check.R
+++ scripts/0_MODIS_check.R
@@ -17,7 +17,7 @@ tree.check <- aggregate(ftree.df$year, by=list(ftree.df$tile), FUN=length)
 summary(tree.check)
 
 tree.check[tree.check$x<max(tree.check$x),] 
-ftree.df[ftree.df$tile=="h33v09","year"]
+# ftree.df[ftree.df$tile=="h07v06","year"]
 # Problematic tiles: 
 # -- h07v06: missing 2011
 # -- h11v12: missing 2013
@@ -29,11 +29,11 @@ tree.check[tree.check$x<max(tree.check$x),"Group.1"]
 
 
 # --------------
-# 2011
+# Processed 2011-2014
 # --------------
-path.2011 <- "/Volumes/Morton_SDM/SurfTemp_MODIS_MODA2/processed/2011/hdf_raw/"
+path.met <- "/Volumes/Morton_SDM/SurfTemp_MODIS_MODA2/processed/2014/hdf_raw/"
 
-fmet.all <- dir(path.2011, ".hdf")
+fmet.all <- dir(path.met, ".hdf")
 fmet.all <- fmet.all[which(substr(fmet.all, nchar(fmet.all)-3, nchar(fmet.all))==".hdf")] # ignore anything that's not a .tif
 
 fmet.split <- stringr::str_split(fmet.all, "[.]")
@@ -47,43 +47,17 @@ summary(fmet.df)
 
 # Check against trees
 tile.check <- aggregate(fmet.df$year, by=list(fmet.df$tile), FUN=length)
+tile.check[tile.check$x < max(tile.check$x) ,]
 summary(tile.check)
 
 tree.check[!tree.check$Group.1 %in% tile.check$Group.1,]
 # 2011 met: h12v05; h18v09
 # --------------
 
-
-# --------------
-# 2012
-# --------------
-path.met <- "/Volumes/Morton_SDM/SurfTemp_MODIS_MODA2/processed/2012/hdf_raw/"
-
-fmet.all <- dir(path.met, ".hdf")
-fmet.all <- fmet.all[which(substr(fmet.all, nchar(fmet.all)-3, nchar(fmet.all))==".hdf")] # ignore anything that's not a .tif
-
-fmet.split <- stringr::str_split(fmet.all, "[.]")
-fmet.df <- data.frame(file=fmet.all, matrix(unlist(fmet.split), ncol=length(fmet.split[[1]]), byrow = T))
-names(fmet.df) <- c("file", "dataset", "date.stamp", "tile", "version", "process.stamp", "extension")
-fmet.df$date <- strptime(as.numeric(substr(fmet.df$date.stamp, 2, 8)), "%Y%j")
-fmet.df$year <- lubridate::year(fmet.df$date)
-fmet.df$month <- lubridate::month(fmet.df$date)
-fmet.df$yday <- lubridate::yday(fmet.df$date)
-summary(fmet.df)
-
-# Check against trees
-tile.check <- aggregate(fmet.df$year, by=list(fmet.df$tile), FUN=length)
-tile.check[tile.check$x < max(tile.check$x) & tile.check$Group.1 %in% tree.check$Group.1,]
-summary(tile.check)
-
-tree.check[!tree.check$Group.1 %in% tile.check$Group.1,]
-# 2011 met: h12v05; h18v09m; h07v06
-# --------------
-
 # --------------
 # 2015
 # --------------
-path.met <- "/Volumes/Morton_SDM/SurfTemp_MODIS_MODA2/FILES_CONVERT/2015/07-08/"
+path.met <- "/Volumes/Morton_SDM/SurfTemp_MODIS_MODA2/FILES_CONVERT/2015/01-02/"
 
 fmet.all <- dir(path.met, ".hdf")
 fmet.all <- fmet.all[which(substr(fmet.all, nchar(fmet.all)-3, nchar(fmet.all))==".hdf")] # ignore anything that's not a .tif
@@ -99,8 +73,8 @@ summary(fmet.df)
 
 # Check against trees
 tile.check <- aggregate(fmet.df$year, by=list(fmet.df$tile), FUN=length)
-tile.check[tile.check$x < max(tile.check$x) & tile.check$Group.1 %in% tree.check$Group.1,]
 summary(tile.check)
+tile.check[tile.check$x < max(tile.check$x) & tile.check$Group.1 %in% tree.check$Group.1,]
 
 tree.check[!tree.check$Group.1 %in% tile.check$Group.1,]
 # 2011 met: h12v05; h18v09
