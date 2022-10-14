d8ca69ae7b2cc853eddfdf42f090bac66b4bf9df
Christy Rollinson <crollinson@gmail.com>
2022-10-14 15:55:54 -0500

    Some script cleanup & bug fixes

--- scripts/201_Extract_Data_EarthEngine.R
+++ scripts/201_Extract_Data_EarthEngine.R
@@ -4,13 +4,71 @@ library(rgee); library(raster); library(rgdal); library(terra)
 # ee_check()
 rgee::ee_Initialize(user = 'crollinson@mortonarb.org', drive=T)
 
-chi <- readOGR("Chicago.shp")
+# chi <- readOGR("Chicago.shp")
+
+##################### 
+# 0. Set up some choices for data quality thresholds
+##################### 
+thresh.sigma <- 6 # Use 6-sigma outliers for the data filtering\
+thresh.pts <- 50
+thresh.prop <- 0.5 # The proportion of data needed for a time point to be "good"; currenlty 0.5
+##################### 
+
 
 ##################### 
 # 0. Set up helper functions
 ##################### 
 addTime <- function(image){
   return(image$addBands(image$metadata('system:time_start')$divide(1000 * 60 * 60 * 24 * 365)))
+}
+
+setNPts <- function(img){
+  npts.now <- img$select("LST_Day_1km")$reduceRegion(reducer=ee$Reducer$count(), geometry=cityNow$geometry(), scale=1e3)
+  p.now <- list(p_Pts=ee$Number(npts.now$get("LST_Day_1km"))$divide(npts.elev))
+  p.now <- ee$Dictionary(p.now)
+  
+  # test <- img$set("n_Pts", npts.now$get("LST_Day_1km"))$set("p_Pts", p.now$get("p_Pts"))
+  # test$get("p_Pts")$getInfo()
+  return(img$set("n_Pts", npts.now$get("LST_Day_1km"))$set("p_Pts", p.now$get("p_Pts")))
+}
+
+lstConvert <- function(img){
+  lstDay <- img$select('LST_Day_1km')$multiply(0.02)
+  lstNight <- img$select('LST_Night_1km')$multiply(0.02)
+  lstK <- ee$Image(c(lstDay, lstNight));
+  img <- img$addBands(srcImg=lstK, overwrite=TRUE);
+  return(img)
+}
+
+# // Cleaning Up and getting just Good LST data
+# // Code adapted from https://gis.stackexchange.com/a/349401/5160
+# // Let's extract all pixels from the input image where
+# // Bits 0-1 <= 1 (LST produced of both good and other quality)
+# // Bits 2-3 = 0 (Good data quality)
+# // Bits 4-5 Ignore, any value is ok
+# // Bits 6-7 = 0 (Average LST error ≤ 1K)
+# // var lstMask = function(qcDay, lstDay){
+# //   var qaMask = bitwiseExtract(qcDay, 0, 1).lte(1)
+# //   var dataQualityMask = bitwiseExtract(qcDay, 2, 3).eq(0)
+# //   var lstErrorMask = bitwiseExtract(qcDay, 6, 7).eq(0)
+# //   var mask = qaMask.and(dataQualityMask).and(lstErrorMask)
+# //   var lstDayMasked = lstDay.updateMask(mask)  
+# // }
+bitwiseExtract <- function(input, fromBit, toBit) {
+  maskSize <- ee$Number(1)$add(toBit)$subtract(fromBit)
+  mask <- ee$Number(1)$leftShift(maskSize)$subtract(1)
+  return(input$rightShift(fromBit)$bitwiseAnd(mask))
+}
+
+lstMask <- function(img){
+  qcDay <- img$select('QC_Day')
+  qaMask <- bitwiseExtract(qcDay, 0, 1)$lte(1);
+  dataQualityMask <- bitwiseExtract(qcDay, 2, 3)$eq(0)
+  lstErrorMask <- bitwiseExtract(qcDay, 6, 7)$lte(2) # setting up error <=2 ˚K
+  datVal <- img$select('LST_Day_1km')$gt(0)
+  maskT <- qaMask$And(dataQualityMask)$And(lstErrorMask)$And(datVal)
+  lstDayMasked <- img$updateMask(maskT)
+  return(lstDayMasked)
 }
 
 
@@ -49,15 +107,6 @@ vizTempAnom <- list(
 
 ##################### 
 
-##################### 
-# 0. Also set up some choices for data quality thresholds
-##################### 
-thresh.sigma <- 6 # Use 6-sigma outliers for the data filtering\
-thresh.pts <- 50
-thresh.prop <- 0.5 # The proportion of data needed for a time point to be "good"; currenlty 0.5
-##################### 
-
-
 
 ##################### 
 # 1. Load and select cities
@@ -87,14 +136,6 @@ citiesBuff <- citiesUse$map(function(f){f$buffer(10e3)})
 # 2.a - Land Surface Temperature
 # -----------
 # 2.a.1 - Northern Hemisphere: July/August
-lstConvert <- function(img){
-  lstDay <- img$select('LST_Day_1km')$multiply(0.02)
-  lstNight <- img$select('LST_Night_1km')$multiply(0.02)
-  lstK <- ee$Image(c(lstDay, lstNight));
-  img <- img$addBands(srcImg=lstK, overwrite=TRUE);
-  return(img)
-}
-
 tempJulAug <- ee$ImageCollection('MODIS/006/MOD11A2')$filter(ee$Filter$dayOfYear(181, 240))$filter(ee$Filter$date("2001-01-01", "2020-12-31"))$map(addTime);
 tempJulAug <- tempJulAug$map(lstConvert)
 # ee_print(tempJulAug$first())
@@ -103,48 +144,15 @@ tempJulAug <- tempJulAug$map(lstConvert)
 # 2.a.2 - Souther Hemisphere: Jan/Feb
 tempJanFeb <- ee$ImageCollection('MODIS/006/MOD11A2')$filter(ee$Filter$dayOfYear(1, 60))$filter(ee$Filter$date("2001-01-01", "2020-12-31"))$map(addTime);
 tempJanFeb <- tempJanFeb$map(lstConvert)
-ee_print(tempJanFeb$first())
+# ee_print(tempJanFeb$first())
 
-# Filtering good LST Data
-# // Cleaning Up and getting just Good LST data
-# // Code adapted from https://gis.stackexchange.com/a/349401/5160
-# // Let's extract all pixels from the input image where
-# // Bits 0-1 <= 1 (LST produced of both good and other quality)
-# // Bits 2-3 = 0 (Good data quality)
-# // Bits 4-5 Ignore, any value is ok
-# // Bits 6-7 = 0 (Average LST error ≤ 1K)
-# // var lstMask = function(qcDay, lstDay){
-# //   var qaMask = bitwiseExtract(qcDay, 0, 1).lte(1)
-# //   var dataQualityMask = bitwiseExtract(qcDay, 2, 3).eq(0)
-# //   var lstErrorMask = bitwiseExtract(qcDay, 6, 7).eq(0)
-# //   var mask = qaMask.and(dataQualityMask).and(lstErrorMask)
-# //   var lstDayMasked = lstDay.updateMask(mask)  
-# // }
-bitwiseExtract <- function(input, fromBit, toBit) {
-  maskSize <- ee$Number(1)$add(toBit)$subtract(fromBit)
-  mask <- ee$Number(1)$leftShift(maskSize)$subtract(1)
-  return(input$rightShift(fromBit)$bitwiseAnd(mask))
-}
-
-
-img <- tempJulAug$first()
-lstMask <- function(img){
-  qcDay <- img$select('QC_Day')
-  qaMask <- bitwiseExtract(qcDay, 0, 1)$lte(1);
-  dataQualityMask <- bitwiseExtract(qcDay, 2, 3)$eq(0)
-  lstErrorMask <- bitwiseExtract(qcDay, 6, 7)$lte(2) # setting up error <=2 ˚K
-  datVal <- img$select('LST_Day_1km')$gt(0)
-  maskT <- qaMask$And(dataQualityMask)$And(lstErrorMask)$And(datVal)
-  lstDayMasked <- img$updateMask(maskT)
-  return(lstDayMasked)
-}
-
+# Filtering good LST Data --> note: we'll still do some outlier remover from each city
 lstDayGoodNH <- tempJulAug$map(lstMask)
 lstDayGoodSH <- tempJanFeb$map(lstMask)
 
 # ee_print(lstDayGoodNH$first())
-# Map$addLayer(lstDayGoodNH$first()$select('LST_Day_1km'), vizTempK, "MASKED Jul/Aug Temperature")
-# Map$addLayer(lstDayGoodSH$first()$select('LST_Day_1km'), vizTempK, "MASKED Jan/Feb Temperature")
+# Map$addLayer(lstDayGoodNH$first()$select('LST_Day_1km'), vizTempK, "GOOD Jul/Aug Temperature")
+# Map$addLayer(lstDayGoodSH$first()$select('LST_Day_1km'), vizTempK, "GOOD Jan/Feb Temperature")
 projLST = lstDayGoodNH$select("LST_Day_1km")$first()$projection()
 # ee_print(projLST)
 # -----------
@@ -153,15 +161,29 @@ projLST = lstDayGoodNH$select("LST_Day_1km")$first()$projection()
 # 2.b MODIS Tree Data
 # -----------
 mod44b = ee$ImageCollection('MODIS/006/MOD44B')$filter(ee$Filter$date("2001-01-01", "2020-12-31"))
-ee_print(mod44b)
+# ee_print(mod44b)
+# Map$addLayer(mod44b$select('Percent_Tree_Cover')$first(), vizTree, 'Percent Tree Cover')
 
+## This worked yesterday, but not today!  :shrug:
+# mod44bReproj = mod44b$map(function(img){
+#   return(img$reduceResolution({ 
+#     reducer=ee$Reducer$mean()
+#   }))$reproject(projLST)
+# })$map(addTime); # add year here!
+
+# This isn't working today.  No f*ng clue hwy
 mod44bReproj = mod44b$map(function(img){
-  return(img$reduceResolution({ 
-    reducer=ee$Reducer$mean()
-  }))$reproject(projLST)
+  return(img$reduceResolution(reducer=ee$Reducer$mean())$reproject(projLST))
 })$map(addTime); # add year here!
-ee_print(mod44bReproj)
-Map$addLayer(mod44bReproj$select('Percent_Tree_Cover')$first(), vizTree, 'Percent Tree Cover')
+
+# mod44bReproj = mod44b$map(function(img){
+#   return(img$reproject(projLST))
+# })$map(addTime); # add year here!
+
+
+# ee_print(mod44bReproj)
+ee_print(mod44bReproj$first())
+# Map$addLayer(mod44bReproj$select('Percent_Tree_Cover')$first(), vizTree, 'Percent Tree Cover')
 # -----------
 
 # -----------
@@ -172,9 +194,11 @@ elev <- ee$Image('USGS/SRTMGL1_003')$select('elevation')
 # Map$addLayer(elev, list(min=-10, max=5e3))
 
 
+# elevReproj <- elev$reduceResolution(reducer=ee$Reducer$mean())$reproject(projLST)
+# 
 elevReproj <- elev$reproject(projLST)
-ee_print(elevReproj)
-# Map$addLayer(elevReproj, list(min=-10, max=5e3))
+# # ee_print(elevReproj)
+# Map$addLayer(elevReproj$select("elevation"), list(min=-10, max=5e3))
 
 # -----------
 ##################### 
@@ -231,7 +255,7 @@ print(citiesList$size()$getInfo())
     return(tmp2$select("system:time_start", "Percent_Tree_Cover", "Percent_NonTree_Vegetation", "Percent_NonVegetated"))
   })
   # ee_print(modCity);
-  # Map$addLayer(modCity$select('Percent_Tree_Cover')$first(), vizTree, 'Percent Tree Cover');y
+  # Map$addLayer(modCity$select('Percent_Tree_Cover')$first(), vizTree, 'Percent Tree Cover');
   
   # Updating our cityMask with MODIS veg data
   cityMask <- cityMask$updateMask(modCity$first()$select("Percent_Tree_Cover", "Percent_NonTree_Vegetation", "Percent_NonVegetated")
@@ -243,7 +267,7 @@ print(citiesList$size()$getInfo())
   # extracting elevation 
   #-------
   elevCity <- elevReproj$updateMask(cityMask)
-  Map$addLayer(elevCity, list(min=-10, max=200))
+  # Map$addLayer(elevCity, list(min=-10, max=5000))
   
   npts.elev <- elevCity$reduceRegion(reducer=ee$Reducer$count(), geometry=cityNow$geometry(), scale=1e3)
   npts.elev <- npts.elev$getInfo()$elevation
@@ -270,21 +294,17 @@ print(citiesList$size()$getInfo())
     tempNow <- tempNow$updateMask(dat)
     return(tempNow)
   })
-  # ee_print(tempCityAll$first())
+  # ee_print(tempCityAll)
+  # Map$addLayer(tempCityAll$first()$select('LST_Day_1km'), vizTempK, "Raw Surface Temperature")
   
   ## ----------------
   ## Need to remove layers that don't meet our requirements right off the bat
   ##   -- because we can't compute means etc when there are no good values
   ##   -- resource from Ren https://gis.stackexchange.com/questions/276791/removing-images-based-on-band-threshold-using-google-earth-engine
   ## ----------------
-  setNPts <- function(img){
-    npts.now <- img$select("LST_Day_1km")$reduceRegion(reducer=ee$Reducer$count(), geometry=cityNow$geometry(), scale=1e3)
-    p.now <- ee$Number(npts.now$get("LST_Day_1km"))$divide(npts.elev)
-    
-    return(img$set("n_Pts", test2$get("n_Pts"))$set("p_Pts", p.now))
-  }
 
   tempCityAll <- tempCityAll$map(setNPts)
+  # ee_print(tempCityAll$first())
   # print(tempCityAll$first()$get("n_Pts")$getInfo())
   # print(tempCityAll$first()$get("p_Pts")$getInfo())
 
@@ -300,23 +320,23 @@ print(citiesList$size()$getInfo())
   ## ----------------
   tempCityAll <- tempCityAll$map(function(img){
     # Calculate the means & sds for the region
-    tempStats <- tempNow$select("LST_Day_1km")$reduceRegion(reducer=ee$Reducer$mean()$combine(
+    tempStats <- img$select("LST_Day_1km")$reduceRegion(reducer=ee$Reducer$mean()$combine(
                                         reducer2=ee$Reducer$stdDev(), sharedInputs=T),
                                       geometry=cityNow$geometry(), scale=1e3)
   
     # Cacluate the key numbers for our sanity
     tmean <- ee$Number(tempStats$get("LST_Day_1km_mean"))
     tsd <- ee$Number(tempStats$get("LST_Day_1km_stdDev"))
-    thresh <- tsd$multiply(temp.sigma)
+    thresh <- tsd$multiply(thresh.sigma)
     
     # Do the filtering
-    dat.low <- tempNow$gte(tmean$subtract(thresh))
-    dat.hi <- tempNow$lte(tmean$add(thresh))
-    tempNow <- tempNow$updateMask(dat.low)
-    tempNow <- tempNow$updateMask(dat.hi)
+    dat.low <- img$gte(tmean$subtract(thresh))
+    dat.hi <- img$lte(tmean$add(thresh))
+    img <- img$updateMask(dat.low)
+    img <- img$updateMask(dat.hi)
 
     # Map$addLayer(tempNow$select('LST_Day_1km'), vizTempK, "Raw Surface Temperature")
-    return(tempNow)
+    return(img)
   })
   # ee_print(tempCityAll)
   # Map$addLayer(tempCityAll$first()$select('LST_Day_1km'), vizTempK, "Raw Surface Temperature")
@@ -327,7 +347,7 @@ print(citiesList$size()$getInfo())
   
   tempCityAll <- tempCityAll$filter(ee$Filter$gte("n_Pts", thresh.pts)) # have at least 50 points (see top of script)
   tempCityAll <- tempCityAll$filter(ee$Filter$gte("p_Pts", thresh.prop)) # Have at least 50% of the data
-  ee_print(tempCityAll)
+  # ee_print(tempCityAll)
   ## ----------------
   
   ## ----------------
