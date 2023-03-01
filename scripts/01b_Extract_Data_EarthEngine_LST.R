3d835a2f2ce4a0880b0564c15f5a1388224842bb
Christy Rollinson <crollinson@mortonarb.org>
2023-03-01 14:45:18 -0600

    Fix broken file path

--- scripts/01b_Extract_Data_EarthEngine_LST.R
+++ scripts/01b_Extract_Data_EarthEngine_LST.R
@@ -7,7 +7,7 @@ library(rgee); library(raster); library(terra)
 ee_check() # For some reason, it's important to run this before initializing right now
 rgee::ee_Initialize(user = 'crollinson@mortonarb.org', drive=T)
 user.google <- dir("~/Library/CloudStorage/")
-path.google <- file.path("~/Library/CloudStorage", user.google)
+path.google <- file.path("~/Library/CloudStorage", user.google, "My Drive")
 GoogleFolderSave <- "UHI_Analysis_Output_Final_v2"
 
 ##################### 
